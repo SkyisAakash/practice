ff86c26fe861f584d4555a9c4526c3746323a0d2
Aakash Sarang <sarangaakash@gmail.com>
2018-10-12 12:04:32 -0700

    Finish maxheap

--- heap.js
+++ heap.js
@@ -11,7 +11,10 @@ class maxHeap {
     }
 
     pop() {
-        return this.store[0]
+        [this.store[0], this.store[this.store.length-1]] = [this.store[this.store.length-1], this.store[0]]
+        let ans = this.store.pop()
+        this.heapifyDown(0)
+        return ans
     }
 
     parentIndex(child) {
@@ -19,7 +22,9 @@ class maxHeap {
     }
 
     childIndices(parent) {
-        return [2*parent+1, 2*parent+2]
+        if(2*parent+2>this.store.length) return []
+        else if(2*parent+2===this.store.length) return [2*parent+1]
+        else return [2*parent+1, 2*parent+2]
     }
 
     heapifyUp(index) {
@@ -30,20 +35,38 @@ class maxHeap {
             this.heapifyUp(parent)
         }
     }
+
+    heapifyDown(index) {
+        let child = this.childIndices(index)
+        let potentialChild;
+        if (child.length===0)return;
+        else if(child.length===1)potentialChild = child[0]
+        else potentialChild = (this.store[child[0]]<=this.store[child[1]]) ? child[1] : child[0] //we want greater because of maxheap
+        if(this.store[potentialChild]<this.store[index])return;
+        else {
+            [this.store[index], this.store[potentialChild]] = [this.store[potentialChild], this.store[index]]
+            this.heapifyDown(potentialChild)
+        }
+    }
 }
+
+module.exports = maxHeap;
 
 let m = new maxHeap()
 m.push(6)
-console.log(m.store)
+// console.log(m.store)
 m.push(11)
-console.log(m.store)
+// console.log(m.store)
 m.push(8)
-console.log(m.store)
+// console.log(m.store)
 m.push(5)
-console.log(m.store)
+// console.log(m.store)
 m.push(10)
-console.log(m.store)
+// console.log(m.store)
 m.push(9)
-console.log(m.store)
+// console.log(m.store)
 m.push(7)
+// console.log(m.store)
+let ans = m.pop()
+console.log(ans)
 console.log(m.store)
